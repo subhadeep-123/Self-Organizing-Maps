99bc2501eb0637a22df61905c5c056ca4f581ccc
Subhadeep Banerjee <subhadeep762@gmail.com>
2019-07-12 02:44:32 +0530

    Updated

--- ann.py
+++ ann.py
@@ -1,16 +1,3 @@
-# Artificial Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
